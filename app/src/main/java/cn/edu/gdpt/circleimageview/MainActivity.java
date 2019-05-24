9817b756e9246c128efa42e8209b1e428068f773
admin <1096187594@qq.com>
2019-05-24 12:00:11 +0800

    这是一个圆形布局

--- app/src/main/java/cn/edu/gdpt/circleimageview/MainActivity.java
+++ app/src/main/java/cn/edu/gdpt/circleimageview/MainActivity.java
@@ -1,13 +1,34 @@
 package cn.edu.gdpt.circleimageview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private CircleImageView circleImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+    }
+
+    private void initView() {
+        circleImageView = (CircleImageView) findViewById(R.id.circleImageView);
+        circleImageView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+         switch (v.getId()){
+             case R.id.circleImageView:
+                 startActivity(new Intent(MainActivity.this,Main2Activity.class));
+         }
     }
 }
